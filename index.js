59a4dcecdc192a8134da28b396a9683ac4331fc3
hardikgoyal1094 <goyalhardik220@gmail.com>
2025-04-04 01:29:10 +0530

    set backend

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const axios = require("axios");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.port || 5000;
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+const cities =["New York", "London", "Tokyo"];
+const crypto = ["bitcoin", "etereum", "dogecoin"];
+
+app.get("/", async (req, res)=>{
+    try{
+        //fetch weather data
+        const weatherPromises = cities.map(city =>
+            axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER_API_KEY}&units=metric`)
+        );
+        const weatherData = await Promise.all(weatherPromises);
+        const weather = weatherData.map((res, index)=>({
+            city: cities[index],
+            temp: res.data.main.temp,
+            humidity: res.data.main.humidity,
+            condition: res.data.weather[0].description 
+        }));
+        
+        // fetch crypto data
+        const cryptoResponse = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${cryptos.join(",")}&vs_currencies=usd&include_market_cap=true&include_24hr_change=true`);
+        const crypto = cryptoResponse.map(coin =>({
+            name: coin,
+            price: cryptoResponse.data[coin].usd,
+            marketCap: cryptoResponse.data[coin].usd_market_cap,
+            change24h: cryptoResponse.data[coin].usd_24h_change.toFixed(2)
+        }));
+
+        //fetch crypto news
+        const newsResponse = await axios.get(`https://newsdata.io/api/1/news?apikey=${process.env.NEWS_API_KEY}&q=cryptocurrency`);
+        const news = newsResponse.data.result.slice(0,5).map(article =>({
+            title: article.title,
+            url: article.link
+        }));
+
+        res.render("index", {weather, crypto, news});
+    } catch(error){
+        console.error("error fetching dashboard data:", error);
+        res.status(500).send("error fetching data.");
+    }
+});
+
+app.listen(port, ()=> console.log(`server is listening at port :${port}`));
